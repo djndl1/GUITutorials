341d7e3012b3f47462501eea9da84eae3bf39740
djndl1 <djndl1@gmail.com>
2022-12-06 22:05:25 +0800

    learn(Win32GUI): HelloWindows comment

--- Win32GUI/src/HelloWindows.c
+++ Win32GUI/src/HelloWindows.c
@@ -1,8 +1,20 @@
 #include <windows.h>
 
+
+/**
+* @brief The entry of Win32 GUI apps
+*
+* called with the __stdcall calling convention
+*
+* @param hInstance the program's instance handle
+* @param hPrevInstance always `NULL`. Initially for multi-instance detection in Win16
+* @param lpCmdLine command line arguments
+* @param nShowCmd how the program should be initially displayed: maximized, minimized or normal
+* @return the error status
+*/
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
 {
-    MessageBoxW(NULL, L"Hello, Windows!", L"Hello Windows", 0);
+    MessageBoxW(NULL, L"Hello, Windows!", L"Hello Windows", MB_YESNO | MB_ICONINFORMATION);
 
     return 0;
 }
