25d1cf09374484916a70ce4552c2cdd1c2f5f418
djndl1 <djndl1@gmail.com>
2022-12-25 01:08:29 +0800

    refactor(GTK): use GNU style for C++ code

--- GTK/gtk_c/src/glib/basics.cc
+++ GTK/gtk_c/src/glib/basics.cc
@@ -1,30 +1,50 @@
-extern "C" {
 #include <glib.h>
-}
 
 #include <gtest/gtest.h>
 
 #include <typeinfo>
 
-struct mystruct {
-    int A;
-    double B;
+struct mystruct
+{
+  int A;
+  double B;
 };
 
-TEST(BASIC_TYPES, OFFSET_STRUCT) {
-    glong offset = G_STRUCT_OFFSET(mystruct, B);
-
-    EXPECT_EQ(offset, 8);
-}
-
-TEST(BASIC_TYPES, GBoolean)
+TEST (BASIC_TYPES, OFFSET_STRUCT)
 {
-    EXPECT_EQ(0, FALSE);
-    EXPECT_EQ(1, TRUE);
+  glong offset = G_STRUCT_OFFSET (mystruct, B);
 
-    EXPECT_EQ(typeid(gboolean), typeid(int));
+  EXPECT_EQ (offset, 8);
 }
 
-TEST(BASIC_TYPES, Alignment) {
-    EXPECT_EQ(8, G_MEM_ALIGN);
+TEST (BASIC_TYPES, GBoolean)
+{
+  EXPECT_EQ (0, FALSE);
+  EXPECT_EQ (1, TRUE);
+
+  EXPECT_EQ (typeid (gboolean), typeid (int));
+}
+
+TEST (BASIC_TYPES, Alignment) { EXPECT_EQ (8, G_MEM_ALIGN); }
+
+TEST (BASIC_TYPES, FloatingPointDecomposition)
+{
+  gfloat f = 1;
+  GFloatIEEE754 fdecomposed = { .v_float = f };
+  EXPECT_EQ (0, fdecomposed.mpn.sign);
+  EXPECT_EQ (0, fdecomposed.mpn.mantissa);
+  EXPECT_EQ (127, fdecomposed.mpn.biased_exponent);
+}
+
+TEST(BASIC_TYPES, GString)
+{
+  GString *s = g_string_new(NULL); // an empty string
+
+  EXPECT_STREQ(s->str, "");
+
+  g_string_append(s, "ABCD中文");
+  EXPECT_EQ(6, g_utf8_strlen(s->str, s->len));
+
+  g_string_free(s, TRUE);
+  s = NULL;
 }
--- GTK/gtk_c/src/glib/environment.cc
+++ GTK/gtk_c/src/glib/environment.cc
@@ -1,131 +1,137 @@
-extern "C" {
-    #include <glib.h>
-}
-
+#include <glib.h>
 #include <gtest/gtest.h>
 
 #include <filesystem>
 
-TEST(ENV, CWD) {
-    const gchar *cwd = g_get_current_dir();
+TEST (ENV, CWD)
+{
+  const gchar *cwd = g_get_current_dir ();
 
-    std::filesystem::path current_path = std::filesystem::current_path();
+  std::filesystem::path current_path = std::filesystem::current_path ();
 
-    g_message("%s\n", cwd);
-    g_message("%s\n", current_path.c_str());
+  g_message ("%s\n", cwd);
+  g_message ("%s\n", current_path.c_str ());
 
-    EXPECT_STREQ(current_path.c_str(), cwd);
+  EXPECT_STREQ (current_path.c_str (), cwd);
 }
 
-TEST(TIME, REAL_TIME) {
-    gint64 current_timestamp = g_get_real_time(); // UTC time
+TEST (TIME, REAL_TIME)
+{
+  gint64 current_timestamp = g_get_real_time (); // UTC time
 
-    GDateTime* current_datetime = g_date_time_new_from_unix_utc(current_timestamp / 1000'000);
-    GDateTime* current_localdatetime = g_date_time_new_from_unix_local(current_timestamp / 1000'000);
+  GDateTime *current_datetime = g_date_time_new_from_unix_utc(current_timestamp / 1000'000);
+  GDateTime *current_localdatetime = g_date_time_new_from_unix_local(current_timestamp / 1000'000);
 
-    gchar *isotime = g_date_time_format_iso8601(current_datetime);
-    gchar *isolocaltime = g_date_time_format_iso8601(current_localdatetime);
+  gchar *isotime = g_date_time_format_iso8601(current_datetime);
+  gchar *isolocaltime = g_date_time_format_iso8601(current_localdatetime);
 
-    g_message("Current UTC time: %s, %ld\n", isotime, current_timestamp);
-    g_message("Current local time: %s at %s\n", isolocaltime, g_date_time_get_timezone_abbreviation(current_localdatetime));
+  g_message("Current UTC time: %s, %ld\n", isotime, current_timestamp);
+  g_message("Current local time: %s at %s\n",
+      isolocaltime,
+      g_date_time_get_timezone_abbreviation(current_localdatetime));
 
-    g_free(isotime);
-    isotime = NULL;
-    g_free(isolocaltime);
-    isolocaltime = NULL;
-    g_date_time_unref(current_datetime);
-    current_datetime = NULL;
-    g_date_time_unref(current_localdatetime);
-    current_localdatetime = NULL;
+  g_free(isotime);
+  isotime = NULL;
+  g_free(isolocaltime);
+  isolocaltime = NULL;
+  g_date_time_unref(current_datetime);
+  current_datetime = NULL;
+  g_date_time_unref(current_localdatetime);
+  current_localdatetime = NULL;
 }
 
-TEST(TIME, GDateTime) {
-    GTimeZone *tz = g_time_zone_new_local();
-    GDateTime *current_datetime = g_date_time_new_now(tz);
-    g_time_zone_unref(tz);
-    tz = NULL;
-    // tz still available since it's refcounted
-    GTimeZone *current_timezone = g_date_time_get_timezone(current_datetime);
-    EXPECT_NE(current_timezone, (std::nullptr_t)NULL);
-    g_message("Current Timezone: %s\n", g_time_zone_get_identifier(current_timezone));
+TEST (TIME, GDateTime)
+{
+  GTimeZone *tz = g_time_zone_new_local ();
+  GDateTime *current_datetime = g_date_time_new_now (tz);
+  g_time_zone_unref (tz);
+  tz = NULL;
+  // tz still available since it's refcounted
+  GTimeZone *current_timezone = g_date_time_get_timezone (current_datetime);
+  EXPECT_NE (current_timezone, (std::nullptr_t)NULL);
+  g_message ("Current Timezone: %s\n",
+             g_time_zone_get_identifier (current_timezone));
 
-    gchar *isotime = g_date_time_format_iso8601(current_datetime);
+  gchar *isotime = g_date_time_format_iso8601 (current_datetime);
 
-    const gchar* tz_abbr = g_date_time_get_timezone_abbreviation(current_datetime);
-    g_message("Current local time: %s %s\n", isotime, tz_abbr);
+  const gchar *tz_abbr
+      = g_date_time_get_timezone_abbreviation (current_datetime);
+  g_message ("Current local time: %s %s\n", isotime, tz_abbr);
 
-    g_free(isotime);
-    isotime = NULL;
+  g_free (isotime);
+  isotime = NULL;
 
-    GDateTime *current_utc = g_date_time_new_now_utc();
-    gchar *iso_utctime = g_date_time_format_iso8601(current_utc);
-    g_message("Current UTC time: %s\n", iso_utctime);
+  GDateTime *current_utc = g_date_time_new_now_utc ();
+  gchar *iso_utctime = g_date_time_format_iso8601 (current_utc);
+  g_message ("Current UTC time: %s\n", iso_utctime);
 
-    g_free(iso_utctime);
-    iso_utctime = NULL;
+  g_free (iso_utctime);
+  iso_utctime = NULL;
 
-    EXPECT_GE(g_date_time_compare(current_utc, current_datetime), 0);
+  EXPECT_GE (g_date_time_compare (current_utc, current_datetime), 0);
 
-    g_date_time_unref(current_datetime);
-    current_datetime = NULL;
-    g_date_time_unref(current_utc);
-    current_utc = NULL;
+  g_date_time_unref (current_datetime);
+  current_datetime = NULL;
+  g_date_time_unref (current_utc);
+  current_utc = NULL;
 }
 
-TEST(TIME, MONO_TIME) {
-    // basically the uptime
-    gint64 then = g_get_monotonic_time();
+TEST (TIME, MONO_TIME)
+{
+  // basically the uptime
+  gint64 then = g_get_monotonic_time ();
 
-    g_usleep(2000);
+  g_usleep (2000);
 
-    gint64 now = g_get_monotonic_time();
+  gint64 now = g_get_monotonic_time ();
 
-    g_message("Monotonic time: %lf hours\n", now / ((gdouble)G_USEC_PER_SEC) / 3600);
+  g_message ("Monotonic time: %lf hours\n",
+             now / ((gdouble)G_USEC_PER_SEC) / 3600);
 
-    EXPECT_GT(now - then, 2000);
+  EXPECT_GT (now - then, 2000);
 }
 
-TEST(DATE, GDATE) {
-    GDate *date = g_date_new_dmy(1, G_DATE_NOVEMBER, 2022);
+TEST (DATE, GDATE)
+{
+  GDate *date = g_date_new_dmy (1, G_DATE_NOVEMBER, 2022);
 
-    gchar formatted_date[15] = { 0 };
-    g_date_strftime(formatted_date, 15, "%F", date);
-    g_message("Date: %s\n",formatted_date);
+  gchar formatted_date[15] = { 0 };
+  g_date_strftime (formatted_date, 15, "%F", date);
+  g_message ("Date: %s\n", formatted_date);
 
-    GDate *old_date = g_date_copy(date);
+  GDate *old_date = g_date_copy (date);
 
-    g_date_add_days(date, 10);
-    g_date_strftime(formatted_date, 15, "%F", date);
-    g_message("Date: %s\n",formatted_date);
+  g_date_add_days (date, 10);
+  g_date_strftime (formatted_date, 15, "%F", date);
+  g_message ("Date: %s\n", formatted_date);
 
-    gint days = g_date_days_between(old_date, date);
-    g_message("Days between: %d\n", days);
+  gint days = g_date_days_between (old_date, date);
+  g_message ("Days between: %d\n", days);
 
-    EXPECT_GE(g_date_compare(date, old_date), 0);
+  EXPECT_GE (g_date_compare (date, old_date), 0);
 
-    g_date_free(date);
-    date = NULL;
-    g_date_free(old_date);
-    old_date = NULL;
+  g_date_free (date);
+  date = NULL;
+  g_date_free (old_date);
+  old_date = NULL;
 }
 
-TEST(DATETIME, TimeZone) {
-    GTimeZone *timezone = g_time_zone_new_local();
+TEST (DATETIME, TimeZone)
+{
+  GTimeZone *timezone = g_time_zone_new_local ();
 
-    gint64 current_utc = g_get_real_time();
-    gint tz_interval = g_time_zone_find_interval(timezone, G_TIME_TYPE_UNIVERSAL, current_utc);
+  gint64 current_utc = g_get_real_time ();
+  gint tz_interval = g_time_zone_find_interval (
+      timezone, G_TIME_TYPE_UNIVERSAL, current_utc);
 
-    const gchar *tz_id = g_time_zone_get_identifier(timezone);
-    const gchar *tz_abbr = g_time_zone_get_abbreviation(timezone, tz_interval);
-    gint32 tz_offset = g_time_zone_get_offset(timezone, tz_interval);
-    gboolean is_dst = g_time_zone_is_dst(timezone, tz_interval);
+  const gchar *tz_id = g_time_zone_get_identifier (timezone);
+  const gchar *tz_abbr = g_time_zone_get_abbreviation (timezone, tz_interval);
+  gint32 tz_offset = g_time_zone_get_offset (timezone, tz_interval);
+  gboolean is_dst = g_time_zone_is_dst (timezone, tz_interval);
 
+  g_message ("Timezone at %s %s with offset %f hours, %s\n", tz_id, tz_abbr,
+             tz_offset / 3600.0, is_dst ? "Day Light Saving" : "non-dst");
 
-    g_message("Timezone at %s %s with offset %f hours, %s\n",
-        tz_id, tz_abbr, tz_offset / 3600.0,
-        is_dst ? "Day Light Saving" : "non-dst");
-
-
-    g_time_zone_unref(timezone);
-    timezone = NULL;
+  g_time_zone_unref (timezone);
+  timezone = NULL;
 }
