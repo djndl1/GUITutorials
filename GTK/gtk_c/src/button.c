5c8b5c338e7e2e14e7e00a53909f23c90fc01650
djndl1 <djndl1@gmail.com>
2022-12-25 18:58:04 +0800

    review(GTK): hello world; button

--- GTK/gtk_c/src/button.c
+++ GTK/gtk_c/src/button.c
@@ -1,41 +1,45 @@
 #include <gtk/gtk.h>
 
-static void destroy(GtkWidget* widget, gpointer data)
-{
-        gtk_main_quit();
+#undef G_LOG_DOMAIN
+#define G_LOG_DOMAIN "gtk_c.button"
+
+static void destroy(GtkWidget *widget, gpointer data) { gtk_main_quit(); }
+static gboolean delete_event(GtkWidget *widget, GdkEvent *event,
+                             gpointer data) {
+  return FALSE;
 }
-static gboolean delete_event(GtkWidget* widget, GdkEvent *event, gpointer data)
-{
-        return FALSE;
-}
 
-int main(int argc, char *argv[])
-{
-        gtk_init(&argc, &argv);
+int main(int argc, char *argv[]) {
+  gtk_init(&argc, &argv);
 
-        // top level windows got WM decorations and a border frame, managed by the WM
-        GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-        gtk_window_set_title(GTK_WINDOW(window), "Hello world");
-        gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-        gtk_widget_set_size_request(window, 200, 100); // sets the minimum size of a widget
+  // top level windows got WM decorations and a border frame, managed by the WM
+  GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "Hello world");
+  gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+  gtk_widget_set_size_request(window, 200,
+                              100);  // sets the minimum size of a widget
+  gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
-        g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL);
-        g_signal_connect(G_OBJECT(window), "delete_event", G_CALLBACK(delete_event), NULL);
+  g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL);
+  g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(delete_event),
+                   NULL);
 
+  GtkWidget *closeButton = gtk_button_new_with_mnemonic("_Close");
+  gtk_button_set_relief(GTK_BUTTON(closeButton), GTK_RELIEF_NORMAL);
+  g_signal_connect_swapped(G_OBJECT(closeButton), "clicked",
+                           G_CALLBACK(gtk_widget_destroy), window);
+  // pass the data into gtk_widget_destory instead of creating a custom callback for the button
+  // just to destroy the window
 
-        GtkWidget *closeButton = gtk_button_new_with_mnemonic("_Close");
-        gtk_button_set_relief(GTK_BUTTON(closeButton), GTK_RELIEF_NORMAL);
-        g_signal_connect_swapped(G_OBJECT(closeButton), "clicked", G_CALLBACK(gtk_widget_destroy), window);
+  gtk_container_add(GTK_CONTAINER(window), closeButton);
 
-        gtk_container_add(GTK_CONTAINER(window), closeButton);
+  // GTK queues the widget until all preprocessing is complete before it
+  // is drawn onto the screen. Not drawn if the parent is not drawn.
+  // The children will be drawn.
+  // the function recursively draws the window, its children and so on
+  gtk_widget_show_all(window);
 
-        // GTK queues the widget until all preprocessing is complete before it
-        // is drawn onto the screen. Not drawn if the parent is not drawn.
-        // The children will be drawn.
-        // the function recursively draws the window, its children and so on
-        gtk_widget_show_all(window);
-
-        // enter the main loop and start processing events
-        gtk_main();
-        return 0;
+  // enter the main loop and start processing events
+  gtk_main();
+  return 0;
 }
--- GTK/gtk_c/src/helloworld.c
+++ GTK/gtk_c/src/helloworld.c
@@ -1,60 +1,81 @@
+#include <glib.h>
 #include <gtk/gtk.h>
 
-static void destroy(GtkWidget* widget, gpointer data)
-{
-        gtk_main_quit();
-}
-static gboolean delete_event(GtkWidget* widget, GdkEvent *event, gpointer data)
-{
-        return FALSE;
-}
+#undef G_LOG_DOMAIN
+#define G_LOG_DOMAIN "gtk_c.helloworld"
 
-static gboolean key_press_callback(GtkWidget *widget,
-                                   GdkEventKey *event,
-                                   GtkWidget *user_data)
-{
-        if (event->keyval == GDK_KEY_F1) {
-                gtk_label_set_text(GTK_LABEL(user_data), "My Name");
-                gtk_window_set_title(GTK_WINDOW(widget), "Deng Hongzhi");
-                return TRUE;
-        }
-        return FALSE;
+static void destroy(GtkWidget *widget, gpointer data) {
+  g_debug("Destroying the main window\n");
+  gtk_main_quit();
+}
+static gboolean delete_event(GtkWidget *widget, GdkEvent *event,
+                             gpointer data) {
+  g_debug("Deleting the main window\n");
+  return FALSE;
 }
 
-int main(int argc, char *argv[])
-{
-        gtk_init(&argc, &argv);
+static gboolean key_press_callback(GtkWidget *widget, GdkEventKey *event,
+                                   GtkWidget *user_data) {
+  static gboolean pressed = FALSE;
+  static gchar *previous = NULL;
+  if (event->keyval == GDK_KEY_F1 && !pressed) {
+    const gchar *label_text = gtk_label_get_text(GTK_LABEL(user_data));
+    if (previous != NULL) {
+      g_free(previous);
+    }
+    previous = g_strdup(label_text);
 
-        // top level windows got WM decorations and a border frame, managed by the WM
-        GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-        gtk_window_set_title(GTK_WINDOW(window), "Hans Meyer");
-        gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-        gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
-        gtk_widget_set_size_request(window, 300, 100); // sets the minimum size of a widget
+    gtk_label_set_text(GTK_LABEL(user_data), "My Name");
+    gtk_window_set_title(GTK_WINDOW(widget), "Deng Hongzhi");
+    pressed = TRUE;
+    return TRUE;
+  } else if (event->keyval == GDK_KEY_F1 && pressed && previous != NULL) {
+    gtk_label_set_text(GTK_LABEL(user_data), previous);
+    pressed = FALSE;
 
-        g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL);
-        g_signal_connect(G_OBJECT(window), "delete_event", G_CALLBACK(delete_event), NULL);
+    return TRUE;
+  }
+  return FALSE;
+}
 
+int main(int argc, char *argv[]) {
+  // set up GTK environment: obtaining GDK display,
+  // preparing the Glib main event loop
+  // basic signal handling
+  gtk_init(&argc, &argv);
 
-        // often used to label other widgets, but also to create large blocks of
-        // uneditable, formatted or wrapped text
-        GtkWidget *label = gtk_label_new("Meyer");
-        gtk_label_set_selectable(GTK_LABEL(label), TRUE);
-        // so that the text can be selected
-        // error messages shoudl be set as selectable
-        g_signal_connect(G_OBJECT(window), "key-press-event",
-                         G_CALLBACK(key_press_callback), label);
+  // top level windows got WM decorations and a border frame, managed by the WM
+  // any window visually seen as a window should be a top level window.
+  GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "Hans Meyer");
+  gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+  gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+  gtk_widget_set_size_request(window, 300,
+                              100);  // sets the minimum size of a widget
 
-        // add the label as a child widget of the window
-        gtk_container_add(GTK_CONTAINER(window), label);
+  g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL);
+  g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(delete_event),
+                   NULL);
 
-        // GTK queues the widget until all preprocessing is complete before it
-        // is drawn onto the screen. Not drawn if the parent is not drawn.
-        // The children will be drawn.
-        // the function recursively draws the window, its children and so on
-        gtk_widget_show_all(window);
+  // often used to label other widgets, but also to create large blocks of
+  // uneditable, formatted or wrapped text
+  GtkWidget *label = gtk_label_new("Meyer");
+  gtk_label_set_selectable(GTK_LABEL(label), TRUE);
+  // so that the text can be selected
+  // error messages shoudl be set as selectable
+  g_signal_connect(G_OBJECT(window), "key-press-event",
+                   G_CALLBACK(key_press_callback), label);
 
-        // enter the main loop and start processing events
-        gtk_main();
-        return 0;
+  // add the label as a child widget of the window
+  gtk_container_add(GTK_CONTAINER(window), label);
+
+  // GTK queues the widget until all preprocessing is complete before it
+  // is drawn onto the screen. Not drawn if the parent is not drawn.
+  // The children will be drawn.
+  // the function recursively draws the window, its children and so on
+  gtk_widget_show_all(window);
+
+  // enter the main loop and start processing events
+  gtk_main();
+  return 0;
 }
