075759d0ccd3c88f4c3d037e2c7c80b2239b324e
djndl1 <djndl1@gmail.com>
2024-03-27 14:42:18 +0000

    add old win32 learning code

---
+++ OldWin32Gui/src/MessageBox.c
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd)
+{
+        MessageBoxW(NULL, L"First Program", L"First", MB_OK);
+
+        return 0;
+}
